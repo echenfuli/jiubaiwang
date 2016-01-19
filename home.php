822da5f1045af125b95b9a2de0aac7c769d974ef
jiubaiwang <9056574@qq.com>
2016-01-19 17:41:19 +0800

    Create home.php

---
+++ home.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+  <head>
+	<?php include'inc/head.php'; ?>
+    <title>选择身份</title>
+    <!-- Custom styles for this template -->
+		<style type="text/css">
+			body {
+			  font-family: "微软雅黑";
+			  padding-top: 40px;
+			  padding-bottom: 40px;
+			  background-color: #eee;
+			}
+			.xuanze{
+				margin-top: 20px;
+			}
+			.xuanze a{
+				margin: 10px 20px 10px 0;
+			}
+		</style>
+  </head>
+  <body>
+	  <div class="container">
+			<h1>请选择进入的身份</h1>
+			<div class="xuanze">
+			  <a href="project_list.php" class="button button-primary button-rounded">售后</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">售前</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">设计师</a>
+			  <a href="project_list.php" class="button button-primary button-rounded"><i class="fa fa-thumbs-up"></i>设计师</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">前端</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">管理员</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">售后</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">售前</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">设计师</a>
+			  <a href="project_list.php" class="button button-primary button-rounded"><i class="fa fa-thumbs-up"></i>设计师</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">前端</a>
+			  <a href="project_list.php" class="button button-primary button-rounded">管理员</a>
+			</div>
+	  </div>
+  </body>
+</html>
